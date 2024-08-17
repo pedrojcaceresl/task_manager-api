58355640dce26b403982f481e0ca42d32e4d5ae6
pedro <pedro.jcaceresl@gmail.com>
2024-08-17 14:10:38 -0400

    modify allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^$&2#j$)6j)p%fb4kznfe*u6dbnk5+u054$0us2t9krb+m_9cm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ".vercel.app"]
 
 
 # Application definition
